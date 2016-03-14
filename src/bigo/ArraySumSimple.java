f4b42140e182b4ee2719820a0d7ad986f6ab3b1e
Irving Chen <irvingsaurus@gmail.com>
2016-03-13 18:25:00 -0700

    Add ArraySum

---
+++ src/bigo/ArraySumSimple.java
@@ -0,0 +1,56 @@
+package bigo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import simplegrammar.*;
+
+public class ArraySumSimple extends ClassDecl {
+
+	public static ClassDecl get() {
+		return new ArraySumSimple();
+	}
+
+	public ArraySumSimple() {
+		super("ArraySum", new ArrayList<>());
+
+		VariableDecl varSumDecl = new VariableDecl(t("int"), "sum");
+		Assignment varSumInitial = new Assignment("sum", lit("0"));
+
+		Assignment varSumLoop = new Assignment("sum",
+				new BinOp("+", l("sum"), l("i")));
+
+		ForEachLoop forEachLoop = new ForEachLoop(l("i"), l("array"), list(varSumLoop));
+
+		Return ret = new Return(l("sum"));
+
+		MethodDecl methodSumDecl = new MethodDecl("sum",
+				list(p("array", "int[]")),
+				list(varSumDecl, varSumInitial, forEachLoop, ret));
+
+		getMethods().add(methodSumDecl);
+	}
+
+	@SafeVarargs
+	private static <E> List<E> list(E ... expressions) {
+		return Arrays.asList(expressions);
+	}
+
+	private static Lookup l(String s) {
+		return new Lookup(s);
+	}
+
+	private static Type t(String s) {
+		return new Type(s);
+	}
+
+	private static Literal lit(String s) {
+		return new Literal(s);
+	}
+
+	private static Parameter p(String n, String t) {
+		return new Parameter(n, t(t));
+	}
+
+}
---
+++ src/sample/ArraySum.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class ArraySum {
+
+	public int sum(int[] array) {
+		int sum = 0;
+		for (int i : array) {
+			sum += i;
+		}
+		return sum;
+	}
+
+}
