b261f0b9b728d3e91d25218a629cbf3619648c7b
Michael Lee <michael.lee.0x2a@gmail.com>
2016-03-15 11:47:29 -0700

    Implement UI

--- src/webserver/Webserver.java
+++ src/webserver/Webserver.java
@@ -4,8 +4,15 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import main.Main;
 import main.ParseRequest;
+import org.apache.commons.lang3.StringEscapeUtils;
 import spark.Request;
 import spark.Response;
+import spark.utils.StringUtils;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import static spark.Spark.*;
 
@@ -18,6 +25,7 @@ public class Webserver {
         post("/parse/full", Webserver::parseFull, Webserver::toJson);
         post("/parse/equation", Webserver::parseEquation, Webserver::toJson);
         post("/parse/partial", Webserver::parsePartial, Webserver::toJson);
+        post("/load", Webserver::loadFile, Webserver::toJson);
 
         after((req, res) -> res.type("application/json"));
     }
@@ -31,7 +39,20 @@ public class Webserver {
     }
 
     public static Object parsePartial(Request request, Response response) {
-        return Main.parsePartial(parseRequest(request.body()).getText());
+        String text = parseRequest(request.body()).getText();
+        System.out.println(text);
+        return Main.parsePartial(text);
+    }
+
+    public static Object loadFile(Request request, Response response) {
+        try {
+            String name = parseRequest(request.body()).getText();
+            String text = readFile("sample/" + name + ".java", Charset.defaultCharset());
+            return new ParseRequest(text);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return new ParseRequest("IOException");
+        }
     }
 
     public static String toJson(Object object) {
@@ -39,6 +60,13 @@ public class Webserver {
     }
 
     public static ParseRequest parseRequest(String text) {
-        return new Gson().fromJson(text, ParseRequest.class);
+        return new Gson().fromJson(StringEscapeUtils.unescapeHtml4(text), ParseRequest.class);
+    }
+
+    static String readFile(String path, Charset encoding)
+            throws IOException
+    {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
     }
 }
