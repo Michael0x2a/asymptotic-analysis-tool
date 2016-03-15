8fa4132b00821ea7458f3e88f38ca7c1287f174e
Phillip Huang <phuang2@cs.washington.edu>
2016-03-14 20:19:09 -0700

    adds sum

---
+++ sample/NestedFor.java
@@ -0,0 +1,18 @@
+public class NestedFor {
+	public void NestedFor(int[] arr) {
+		int i, j;
+		for (i = 0; i < arr.length; i++) {
+			for (j = 0; j < i; j++) {
+				if (arr[j] < arr[minIndex])
+					minIndex = j;
+				else {
+					System.out.println("Hello!");
+				}
+			}
+			// Swap smallest found with element in position i
+			temp = arr[minIndex];
+			arr[minIndex] = arr[i];
+			arr[i] = temp;
+		}
+	}
+}
